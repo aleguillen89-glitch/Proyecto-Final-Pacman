8b74e7b0463e0c6dcf3d7b966f5d56bedf150691
Guillén <fabricioguillen180607@gmail.com>
2026-06-22 18:32:31 -0500

    Muros

---
+++ src/Muro.java
@@ -0,0 +1,22 @@
+public class Muro {
+    // Atributos
+    private int fila;
+    private int columna;
+
+    public Muro( int fila, int columna){
+        this.fila = fila;
+        this.columna = columna;
+    }
+    public String ObtenerPosicion () {
+        return "(" + this.fila + "," + this.columna + ")";
+    }
+    public int getFila () {
+        return this.fila;
+    }
+    public int getColumna () {
+        return this.columna;
+    }
+    public String toString () {
+        return " Muro " + ObtenerPosicion();
+    }
+}
