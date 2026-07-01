e3a7e5435d007d7a280bfbb19e3f4f54e18ff45f
Acosta <aldhairanghela@gmail.com>
2026-06-30 23:52:07 -0500

    JUgador y Enemigo

--- src/Enemigo.java
+++ src/Enemigo.java
@@ -2,57 +2,138 @@ public class Enemigo {
     // Atributos
     private String tipo;
     private String nombre;
-    private String vida;
-    private int DanodeAtaque;
-    private String Armadura;
-    private String Aleatorio;
-    private int Experiencia;
-    private int Fila;
-    private int Columna;
-    private boolean Activo;
-    private String Atravesar;
+    private int vida;
+    private int danodeAtaque;
+    private int armadura;
+    private int experiencia;
+    private int fila;
+    private int columna;
+    private boolean activo;
+
+
+    // Constructor
+    public Enemigo(String tipo, String nombre, int fila, int columna, int danoDeAtaque) {
+        this.tipo = tipo;
+        this.nombre = nombre;
+        this.fila = fila;
+        this.columna = columna;
+        this.danodeAtaque = danoDeAtaque;
+        this.experiencia = 0;
+        this.activo = true;
+        this.vida = 3;
+        this.armadura = 0;
+
+        // Stats iniciales según el tipo de enemigo
+        switch (tipo) {
+            case "Fantasma":
+                // atraviesa muros
+                break;
+            case "Armadura":
+                // no atraviesa muros, tiene escudo
+                this.armadura = 1; //resiste un instakill antes de morir
+                break;
+            case "Perseguidor":
+            case "Aleatorio":
+            default:
+                this.armadura = 0;
+        }
+    }
+
 
     // Metodos
-    public void atacar() { }
-    public void mover() { }
-    public void mostrarEstado() { }
-    public void verificarColision() { }
-    public void esquivar() { }
-    public void recibirDano() { }
-    public void recibirArmadura() { }
-    public void morir() { }
-    public void perseguirJugador() { }
-    public void patrullar() { }
+    public void atacar(Jugador j) {
+        if (this.activo && j.estaVivo()){
+            j.recibirDano(this.danodeAtaque);
+        }
+    }
+    public void mover(Jugador j) {
+        if (!this.activo)return;
+        switch (this.tipo) {
+            case "Perseguidor":
+                perseguirJugador(j);
+                break;
+            case "Aleatorio":
+                aleatorio();
+                break;
+            case "Fantasma":
+                atravesar(j);
+                break;
+            case "Armadura":
+                aleatorio();
+                break;
+        }
+    }
+    public void mostrarEstado() {
+        System.out.println("Tipo: " + tipo + " | Nombre: " + nombre + " | Vida: " + vida + " | Posicion: (" + fila + ", " + columna + ")" + " | Activo: " + activo);
+    }
+    public boolean verificarColision(Jugador j) {
+        return this.activo && this.fila == j.getFila() && this.columna == j.getColumna();
+    }
+    public void morir() {
+        this.activo = false;
+    }
+    public void perseguirJugador(Jugador j) {
+        if (this.fila < j.getFila()) this.fila++;
+        else if (this.fila > j.getFila()) this.fila--;
+
+        if (this.columna < j.getColumna()) this.columna++;
+        else if (this.columna > j.getColumna()) this.columna--;
+    }
+    public void aleatorio() {
+        int direccion = (int) (Math.random() * 4); // 0=arriba,1=abajo,2=izq,3=der
+        switch (direccion) {
+            case 0: this.fila--; break;
+            case 1: this.fila++; break;
+            case 2: this.columna--; break;
+            case 3: this.columna++; break;
+        }
+    }
+    public void atravesar(Jugador j) {
+        perseguirJugador(j);
+    }
+    public void recibirInstakill(){
+        if (this.armadura > 0) {
+            this.armadura--; //pierde la resistencia pero sobrevive
+        } else {
+            morir(); //sin resistencia, muere directo
+        }
+    }
+
 
     // Get
-    public void setnombre (String nombre) { this.nombre = nombre; }
-    public void setvida(String vida) { this.vida = vida; }
-    public void setArmadura(String Armadura) { this.Armadura = Armadura; }
-    public void setDanodeAtaque(int DanodeAtaque) {this.DanodeAtaque = DanodeAtaque; }
-    public void setActivo(boolean Activo) { this.Activo = Activo; }
-    public void setAleatorio(String Aleatorio) { this.Aleatorio = Aleatorio; }
-    public void setAtravesar(String Atravesar) { this.Atravesar = Atravesar; }
+    public void setNombre (String nombre) { this.nombre = nombre; }
+    public void setVida(int vida) { this.vida = vida; }
+    public void setArmadura(int armadura) { this.armadura = armadura; }
+    public void setDanodeAtaque(int danodeAtaque) {this.danodeAtaque = danodeAtaque; }
+    public void setActivo(boolean activo) { this.activo = activo; }
+    public void setFila(int fila) { this.fila = fila; }
+    public void setColumna(int columna) { this.columna = columna; }
+    public void setTipo(String tipo) { this.tipo = tipo; }
 
-    public String getnombre() {
+
+    public String getNombre() {
         return this.nombre;
     }
-    public String getvida() {
+    public int getVida() {
         return this.vida;
     }
-    public String getArmadura() {
-        return Armadura;
+    public int getArmadura() {
+        return armadura;
     }
     public int getDanodeAtaque() {
-        return DanodeAtaque;
+        return danodeAtaque;
     }
     public boolean getActivo() {
-        return Activo;
+        return activo;
     }
-    public String getAleatorio() {
-        return Aleatorio;
+    public int getFila() {
+        return this.fila;
     }
-    public String getAtravesar() {
-        return Atravesar;
+    public int getColumna() {
+        return this.columna;
+    }
+    public String getTipo() {
+        return this.tipo;
     }
 
 }
--- src/Jugador.java
+++ src/Jugador.java
@@ -1,11 +1,10 @@
 public class Jugador {
     //Atributos
     private String nombre;
-    private String vida;
+    private int vida;
     private int danoataque;
-    private String armadura;
-    private String velocidad;
-    private int posicion;
+    private int armadura;
+    private int velocidad;
     private int fila;
     private int columna;
     private int puntaje;
@@ -14,27 +13,39 @@ public class Jugador {
     //Metodos
     public void usarPoder() { }
     public void mostrarEstado() { }
-    public void estaVivo() { }
+    public boolean estaVivo() { return vida>0; }
     public void mover() { }
     public void esquivar() { }
-    public void recibirDaño() { }
+    public void recibirDano(int danodeAtaque) { }
     public void recibirArmadura() { }
     public void morir() { }
     public void recogerPunto() { }
-
+    public Jugador (String nombre, int fila, int columna) {
+        vida=3;
+        velocidad=1;
+        puntaje=0;
+        poderActivo=false;
+    }
 
     //Get
     public void setNombre(String nombre) { this.nombre = nombre; }
-    public void setVida(String vida) { this.vida = vida; }
+    public void setVida(int vida) { this.vida = vida; }
     public void setDanoAtaque(int danoAtaque) { this.danoataque = danoAtaque; }
-    public void setArmadura(String Armadura) { this.armadura = Armadura; }
-    public void setVelocidad(String velocidad) { this.velocidad = velocidad; }
-    public void setPosicion(int Posicion) { this.posicion = Posicion; }
+    public void setArmadura(int armadura) { this.armadura = armadura; }
+    public void setVelocidad(int velocidad) { this.velocidad = velocidad; }
+    public void setFila(int fila) { this.fila = fila; }
+    public void setColumna(int columna) { this.columna = columna; }
+    public void setPuntaje(int puntaje) { this.puntaje = puntaje; }
+    public void setPoderActivo(boolean poderActivo) { this.poderActivo = poderActivo; }
 
     public String getNombre() { return this.nombre; }
-    public String getVida() { return this.vida; }
+    public int getVida() { return this.vida; }
     public int getDanoAtaque() { return this.danoataque; }
-    public String getArmadura() { return this.armadura; }
-    public String getVelocidad() { return this.velocidad; }
-    public int getPosicion() { return this.posicion; }
+    public int getArmadura() { return this.armadura; }
+    public int getVelocidad() { return this.velocidad; }
+    public int getFila() { return this.fila; }
+    public int getColumna() { return this.columna; }
+    public int getPuntaje() { return this.puntaje; }
+    public boolean getPoderActivo() { return this.poderActivo; }
+
 }
